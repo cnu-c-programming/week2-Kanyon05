4a3adaf01aec58643b8184c4919a77f697e67ffe
Kanyon05 <windtree01@gmail.com>
2026-03-13 12:06:22 +0900

    Refactor prime number check in main function
    Fix prime number checking logic and improve formatting.

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    
+    int n;
+    boolean isPrime = true;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    
+    printf("%s", isPrime ? "true" : "false");
 
     return 0;
 }
-

f53b8057e4a1ea418e1c483b3e36442f4cc6fc08
Kanyon05 <windtree01@gmail.com>
2026-03-13 12:08:54 +0900

    Change boolean type to _Bool for isPrime variable

--- code/4-3.c
+++ code/4-3.c
@@ -3,18 +3,18 @@
 int main() {
     
     int n;
-    boolean isPrime = true;
+    _Bool isPrime = true;
 
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 2; i < n; i++) {
         if (n % i == 0) {
             isPrime = false;
             break;
         }
     }
 
-    
+
     printf("%s", isPrime ? "true" : "false");
 
     return 0;
